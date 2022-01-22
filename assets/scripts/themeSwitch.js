0f5287cc882b41e5a9d17e360376f4b839f47b90
RemasteredArch <remasteredarch@gmail.com>
2022-01-21 22:36:12 -0800

    New website

---
+++ assets/scripts/themeSwitch.js
@@ -0,0 +1,38 @@
+// Variables
+const moon = document.getElementById('themeSwitcherMoon')
+const sun = document.getElementById('themeSwitcherSun')
+var theme = "light"
+
+// Show the moon symbol
+function moonShow() {
+  sun.style.display = "none"
+  moon.style.display = "block"
+}
+
+// Show the sun symbol
+function sunShow() {
+  moon.style.display = "none"
+  sun.style.display = "block"
+}
+
+
+// Check if user prefers dark mode
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark')
+  theme = "dark"
+  sunShow()
+}
+// Swap between light and dark mode
+function themeSwitch() {
+  if (theme == "light") {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    theme = "dark"
+    sunShow()
+  } else {
+    document.documentElement.removeAttribute('data-theme')
+    theme = "light"
+    moonShow()
+  }
+}
+
+// if (theme == "dark") { themeSwitch() } // TEMP: Light mode design first
